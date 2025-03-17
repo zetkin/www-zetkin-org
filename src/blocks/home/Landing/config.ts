41ac4f074128ca2c58574a86e86c6c1cf3d6359c
theo-organism <nordahltheo@gmail.com>
2025-03-17 19:11:19 -0300

    Ran prettier and fixed build issues
    Removing links also from hero components, update button use in not found page

--- src/blocks/home/Landing/config.ts
+++ src/blocks/home/Landing/config.ts
@@ -2,55 +2,54 @@ import type { Block } from 'payload';
 
 import { link } from '@/fields/link';
 
-
 export const Landing: Block = {
   slug: 'landing',
   interfaceName: 'LandingBlock',
   fields: [
     {
-        name: 'leftTitle',
-        type: 'text',
-        label: 'Left title',
-        required: true,
+      name: 'leftTitle',
+      type: 'text',
+      label: 'Left title',
+      required: true,
     },
     {
-        name: 'rightTitle',
-        type: 'text',
-        label: 'Right title',
-        required: true,
+      name: 'rightTitle',
+      type: 'text',
+      label: 'Right title',
+      required: true,
     },
     {
-        name: 'subtitle',
-        type: 'text',
-        label: 'Sub-title',
-        required: true,
+      name: 'subtitle',
+      type: 'text',
+      label: 'Sub-title',
+      required: true,
     },
     {
-        name: 'buttons',
-        type: 'array',
-        fields: [
+      name: 'buttons',
+      type: 'array',
+      fields: [
+        {
+          name: 'label',
+          type: 'text',
+          label: 'Button label',
+          required: true,
+        },
+        {
+          name: 'variant',
+          type: 'select',
+          options: [
             {
-                name: 'label',
-                type: 'text',
-                label: 'Button label',
-                required: true,
+              label: 'Primary',
+              value: 'primary',
             },
             {
-                name: 'variant',
-                type: 'select',
-                options: [
-                    {
-                        label: 'Primary',
-                        value: 'primary',
-                    },
-                    {
-                        label: 'Outline',
-                        value: 'outline',
-                    },
-                ]        
+              label: 'Outline',
+              value: 'outline',
             },
-            link(),
-        ],
-    }
+          ],
+        },
+        link(),
+      ],
+    },
   ],
 };
--- src/heros/MediumImpact/index.tsx
+++ src/heros/MediumImpact/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 import type { Page } from '@/payload-types';
-import { CMSLink } from '@/components/Link';
 import { Media } from '@/components/Media';
 import RichText from '@/components/RichText';
 
 export const MediumImpactHero: React.FC<Page['hero']> = ({
-  links,
   media,
   richText,
 }) => {
@@ -15,18 +13,6 @@ export const MediumImpactHero: React.FC<Page['hero']> = ({
       <div className="container mb-8">
         {richText && (
           <RichText className="mb-6" data={richText} enableGutter={false} />
-        )}
-
-        {Array.isArray(links) && links.length > 0 && (
-          <ul className="flex gap-4">
-            {links.map(({ link }, i) => {
-              return (
-                <li key={i}>
-                  <CMSLink {...link} />
-                </li>
-              );
-            })}
-          </ul>
         )}
       </div>
       <div className="container ">
