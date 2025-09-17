54b0bd89054e5a6d0abcb54020c0bac2c45d0be8
theo-organism <nordahltheo@gmail.com>
2025-09-17 12:52:31 -0300

    overlap issues gradient block

--- src/blocks/home/Gradient/Layouts/LeftAligned1.tsx
+++ src/blocks/home/Gradient/Layouts/LeftAligned1.tsx
@@ -30,7 +30,7 @@ export default function LeftAligned1({
         </div>
       </div>
       <div className="h-[105vw] md:h-[40vw] md:w-full relative">
-        <div className="object-cover absolute h-[45vw] w-[82vw] -left-1/12 md:z-10 md:left-auto md:top-[max(369px,50%)] md:right-[-115px] md:w-[45vw] md:max-w-[485px] md:h-[20vw] md:max-h-[241px]">
+        <div className="object-cover absolute h-[45vw] w-[82vw] -left-1/12 md:z-10 md:left-auto md:top-[min(369px,50%)] md:right-[-115px] md:w-[45vw] md:max-w-[485px] md:h-[20vw] md:max-h-[241px]">
           {images[0]?.image && (
             <ImageMedia
               fill
